545c71269a747add4268a9b6bb54e3996d9b07ef
David Benko <dbenko@anypresence.com>
2015-02-11 13:16:46 -0500

    Add HS256 Algorithm

---
+++ JWT/JWTAlgorithmHS256.h
@@ -0,0 +1,14 @@
+//
+//  JWTAlgorithmHS256.h
+//  JWT
+//
+//  Created by David Benko on 2/11/15.
+//  Copyright (c) 2015 Karma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "JWTAlgorithm.h"
+
+@interface JWTAlgorithmHS256 : NSObject <JWTAlgorithm>
+
+@end
